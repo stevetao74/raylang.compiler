558e16e764599435cbb30fc079f46deee747fee7
ScSofts <3416616002@qq.com>
2020-09-27 10:34:18 +0800

    hard trying to bind llvm

--- v2/codegen.cpp
+++ v2/codegen.cpp
@@ -8,7 +8,16 @@ using namespace std;
 static llvm::LLVMContext TheContext;
 static llvm::IRBuilder<> Builder(TheContext);
 static std::unique_ptr<llvm::Module> TheModule;
-static std::map<std::string, llvm::Value *> NamedValues;
+static std::map<std::string, llvm::AllocaInst*> NamedValues;
+/// CreateEntryBlockAlloca - Create an alloca instruction in the entry block of
+/// the function.  This is used for mutable variables etc.
+static llvm::AllocaInst *CreateEntryBlockAlloca(llvm::Function *TheFunction,
+                                          const std::string &VarName) {
+  llvm::IRBuilder<> TmpB(&TheFunction->getEntryBlock(),
+                 TheFunction->getEntryBlock().begin());
+  return TmpB.CreateAlloca(llvm::Type::getDoubleTy(TheContext), 0,
+                           VarName.c_str());
+}
 
 llvm::Value *IntegerAST::CodeGen(){
     return llvm::ConstantInt::get(TheContext,llvm::APInt( 64,uint64_t(value) ));
@@ -16,6 +25,9 @@ llvm::Value *IntegerAST::CodeGen(){
 llvm::Value *DoubleAST::CodeGen(){
     return llvm::ConstantFP::get( TheContext,llvm::APFloat(value) );
 }
-llvm Value *VariableDeclarationAST::CodeGen(){
-    return llvm::
+llvm::Value *VariableDeclarationAST::CodeGen(){
+    llvm::Value *V = NamedValues[this->id.name];
+    if(!V){
+        
+    }
 }
